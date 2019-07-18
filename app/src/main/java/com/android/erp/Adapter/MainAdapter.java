bba93850bdd443628cba05b89bdfbe30b2c6a207
Talib <rookiegaming55@gmail.com>
2019-07-19 03:12:06 +0400

    mirzemehdi

--- app/src/main/java/com/android/erp/Adapter/MainAdapter.java
+++ app/src/main/java/com/android/erp/Adapter/MainAdapter.java
@@ -9,6 +9,7 @@ import android.view.View;
 import android.view.ViewGroup;
 
 import com.android.erp.CategoriesActivity;
+import com.android.erp.Models.PagerModel;
 import com.android.erp.Models.TitleChild;
 import com.android.erp.Models.TitleParent;
 import com.android.erp.R;
@@ -17,6 +18,7 @@ import com.android.erp.ViewHolders.TitleParentViewHolder;
 import com.thoughtbot.expandablerecyclerview.ExpandableRecyclerViewAdapter;
 import com.thoughtbot.expandablerecyclerview.models.ExpandableGroup;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class MainAdapter extends ExpandableRecyclerViewAdapter<TitleParentViewHolder, TitleChildViewHolder> {
@@ -48,11 +50,49 @@ public class MainAdapter extends ExpandableRecyclerViewAdapter<TitleParentViewHo
         final TitleChild child = ((TitleParent) group).getItems().get(childIndex);
         holder.setViews(child.getName(),child.getDone(),child.getUndone(),avenir_book);
         holder.itemView.setOnClickListener(v -> {
-            Log.d("sdasdas","flat:" + flatPosition + "   " + "child:"+childIndex);
-            Intent intent = new Intent(context, CategoriesActivity.class);
-            intent.putExtra("userId","1");
-            intent.putExtra("categoryId",String.valueOf(flatPosition));
-            context.startActivity(intent);
+            if (flatPosition ==1 || flatPosition == 2 || flatPosition == 3 || flatPosition ==4){
+                ArrayList<String> pagerModels = new ArrayList<>();
+                ArrayList<String> pagerModelss = new ArrayList<>();
+                pagerModels.add("Twitter");
+                pagerModelss.add("https://images.vexels.com/media/users/3/137419/isolated/preview/b1a3fab214230557053ed1c4bf17b46c-twitter-icon-logo-by-vexels.png");
+                pagerModels.add("Instagram");
+                pagerModelss.add("http://pluspng.com/img-png/instagram-png-instagram-png-logo-1455.png");
+                pagerModels.add("Facebook");
+                pagerModelss.add("https://images.vexels.com/media/users/3/137253/isolated/preview/90dd9f12fdd1eefb8c8976903944c026-facebook-icon-logo-by-vexels.png");
+                pagerModels.add("Linkedin");
+                pagerModelss.add("https://images.vexels.com/media/users/3/137382/isolated/preview/c59b2807ea44f0d70f41ca73c61d281d-linkedin-icon-logo-by-vexels.png");
+                Intent intent = new Intent(context, CategoriesActivity.class);
+                intent.putExtra("myList",pagerModels);
+                intent.putExtra("myLists",pagerModelss);
+                intent.putExtra("userId","1");
+                intent.putExtra("categoryId",String.valueOf(flatPosition));
+                context.startActivity(intent);
+            }else if (flatPosition == 6 || flatPosition == 7){
+                ArrayList<String> pagerModels = new ArrayList<>();
+                ArrayList<String> pagerModelss = new ArrayList<>();
+                pagerModels.add("Photo");
+                pagerModelss.add("https://www.sccpre.cat/mypng/full/15-155680_camera-icon-white-small-white-camera-icon-transparent.png");
+                pagerModels.add("Video");
+                pagerModelss.add("http://www.sclance.com/pngs/video-play-button-transparent-png/video_play_button_transparent_png_1463609.png");
+                Intent intent = new Intent(context, CategoriesActivity.class);
+                intent.putExtra("myList",pagerModels);
+                intent.putExtra("myLists",pagerModelss);
+                intent.putExtra("userId","1");
+                intent.putExtra("categoryId",String.valueOf(flatPosition));
+                context.startActivity(intent);
+            }else if (flatPosition == 9){
+                ArrayList<String> pagerModels = new ArrayList<>();
+                ArrayList<String> pagerModelss = new ArrayList<>();
+                pagerModels.add("SMS");
+                pagerModelss.add("https://vires.com/wp-content/uploads/2016/11/Mail-Icon-white.png");
+                Intent intent = new Intent(context, CategoriesActivity.class);
+                intent.putExtra("myList",pagerModels);
+                intent.putExtra("myLists",pagerModelss);
+                intent.putExtra("userId","1");
+                intent.putExtra("categoryId",String.valueOf(flatPosition));
+                context.startActivity(intent);
+            }
+
         });
     }
 
